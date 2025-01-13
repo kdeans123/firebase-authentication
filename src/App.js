80146d3598eb71e2d441b50379d5ec1a46862e46
Kat <kat.m.deans@gmail.com>
2025-01-13 14:51:17 -0500

    Loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
 import { auth } from '.firebase/init';
 import { 
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword, 
+  signOut,
+  onAuthStateChanged,
 } from '.firebase/auth';
 
 import './App.css';
 
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+React.useEffect(() => {
+  onAuthStateChanged(auth, (user) => {
+    setLoading(false);
+    if (user) {
+      setUser(user)
+    }
+  })
+}, [])
+
   function register() {
     createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
       .then((user) => {
@@ -19,8 +34,8 @@ function App() {
 
   function login() {
     signInWithEmailAndPassword(auth, 'email@email.com', 'test123')
-      .then((user) => {
-        console.log(user);
+      .then((data) => {
+        setUser(user);
       })
       .catch((error) => {
         setErrorMessage('The password is invalid or the user does not have a password')
@@ -28,11 +43,18 @@ function App() {
       })
   }
 
+  function logout() {
+    signOut(auth);
+    setUser({});
+  }
+
 
   return (
     <div className="App">
       <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? 'loading...' : user.email}
     </div>
   );
 }
@@ -46,3 +68,5 @@ export default App;
 // the catch will run when for example ther eis alreday an email registered, if password is not long enough 
 // when you do it in real project you do not hard code email, you want to use hooks 
 // const[email, setEmail] and then you will have an input field that sets the email 
+//  when you first load the page, you need to pass an empty array 
+// onAuthStateChanged - so when the page loads user does not hav eto log in again 
